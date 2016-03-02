d1c7291ae4297257aef7bf8af507e35d31d3e387
Adrian <alu0100288216@ull.edu.es>
2016-03-02 19:18:06 +0000

    Usando XRegExp, primera aproximación.

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,6 +1,8 @@
 (function(exports) {
   "use strict";
 
+  var XRegExp = require('xregexp');
+
   function Medida(valor,tipo){
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     if(!tipo){
@@ -53,12 +55,22 @@
   exports.Farenheit = Farenheit;
   exports.Kelvin = Kelvin;
 
+ // 32C to K
   exports.convertir = function() {
     var valor     = document.getElementById('convert').value,
         elemento  = document.getElementById('converted'),
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i,
         valor     = valor.match(regexp);
+
+              /* '(?<tipo1> [a-z,A-Z]+)   # tipo1 \n' +
+               '(?<to>    [to])?     # to \n' +
+               '(?<tipo2> [a-z,A-Z]+'*/
+        // Using named capture and flag x (free-spacing and line comments)
+    var exp = XRegExp('(?<numero> \\d+)\\s*  # numero  \n' +
+                      '(?<from> [a-z,A-Z]+)\\s+  # tipo1 \n' +
+                      '(?<to> to\\s+)?' +
+                      '(?<tipo2> [a-z,A-Z])+ # tipo2 \n', 'x');
 
     if (valor) {
       var numero = valor[1],
